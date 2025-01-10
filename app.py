13394924e5b85a82e5f95e3d12abedae5ce07818
alaysiacymbia <alaysiamillerchambli@spelman.edu>
2025-01-10 19:19:14 +0000

    checkpoint for table creation and second page

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def root():
     return render_template("base.html")
+
+@app.route('/toxicity/')
+def toxicity():
+    return render_template('toxicity.html')
+
